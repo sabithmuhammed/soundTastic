0a93b774f6f2e1463e9daadf90cdabded4f1cf06
sabithmuhammed <91025942+sabithmuhammed@users.noreply.github.com>
2024-01-02 20:21:03 +0530

    change order status modal bug fixed

--- assets/js/utils/manageOrder.js
+++ assets/js/utils/manageOrder.js
@@ -1,9 +1,8 @@
-
 const changeStatus = document.querySelector("[data-statusBtn]");
 const modal = document.getElementById("confirm-modal");
-const orderCancelModal = null
+const orderCancelModal = null;
 const statusModal = document.getElementById("change-status-modal");
-const changeStatusModal = new bootstrap.Modal(statusModal);
+let changeStatusModal = null;
 const cancelReqBtn = document.querySelector("[data-cancelReqBtn]");
 const statusCol = document.querySelectorAll("[data-status]");
 const changeStatusBtn = document.querySelector("[data-changeStatusBtn]");
@@ -13,45 +12,44 @@ const showCancelModal = () => {
   orderCancelModal.show();
 };
 
-
 const showChangeStatus = () => {
+  changeStatusModal = new bootstrap.Modal(statusModal);
   changeStatusModal.show();
 };
-const changeStatusReq=async(event)=>{
-    try {
-        console.log("ahgdkjas");
-        const orderId=event.currentTarget.dataset.id;
-        const status=document.querySelector('[data-select]').value
-        const error=document.querySelector('[data-statusError]')
-        const rawData=await fetch("/admin/change-status",{
-            method:"PATCH",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({orderId,status})
-        })
-        if(rawData.status===401){
-          window.location.href="/admin/login"
-        }
-        if(rawData.ok){
-            const data =await rawData.json();
-            if(data.status==="success"){
-                statusCol.forEach((item)=>{
-                  item.innerText=data.orderStatus;
-                })
-                status.value=data.orderStatus;
-                if(data.orderStatus==="Delivered"){
-                  document.querySelector('[data-status-tr]').innerHTML=''
-                }
-                changeStatusModal.hide();
-                return
-            }
-        }
-        
-    } catch (error) {
-    console.error(error.message);
+const changeStatusReq = async (event) => {
+  try {
+    console.log("ahgdkjas");
+    const orderId = event.currentTarget.dataset.id;
+    const status = document.querySelector("[data-select]").value;
+    const error = document.querySelector("[data-statusError]");
+    const rawData = await fetch("/admin/change-status", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ orderId, status }),
+    });
+    if (rawData.status === 401) {
+      window.location.href = "/admin/login";
     }
-}
+    if (rawData.ok) {
+      const data = await rawData.json();
+      if (data.status === "success") {
+        statusCol.forEach((item) => {
+          item.innerText = data.orderStatus;
+        });
+        status.value = data.orderStatus;
+        if (data.orderStatus === "Delivered") {
+          document.querySelector("[data-status-tr]").innerHTML = "";
+        }
+        changeStatusModal.hide();
+        return;
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 changeStatus.addEventListener("click", showChangeStatus);
 changeStatusBtn.addEventListener("click", changeStatusReq);
